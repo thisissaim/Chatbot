c81e1f95bde1fe5b08beaa3b9503c52f9bd39001
thisissaim <saimthis1997@gmail.com>
2023-06-01 23:41:04 +0530

    Adding changes

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+  # # message entry box
+        # self.msg_entry = Entry(bottom_label, bg="#2C3E50", fg=TEXT_COLOR, font=FONT)
+        # self.msg_entry.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
+        # self.msg_entry.focus()
+        # self.msg_entry.bind("<Return>", self._on_enter_pressed)
+        
+        # # send button
+        # send_button = Button(bottom_label, text="Send", font=FONT_BOLD, width=20, bg=BG_GRAY,
+        #                      command=lambda: self._on_enter_pressed(None))
+        # send_button.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
